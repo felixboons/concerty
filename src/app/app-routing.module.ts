87403412fe0168b5696b50a6ef1fcf7f2878c0cc
ALTERNATE\Felix <felixboons@hotmail.com>
2019-07-12 18:43:26 +0200

    spawned some elements, created some styles using a lime/blue theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ArtistsComponent} from './components/artists/artists.component';
+import {ArtistDetailsComponent} from './components/artists/artist-details/artist-details.component';
+import {ConcertsComponent} from './components/concerts/concerts.component';
+import {ConcertDetailsComponent} from './components/concerts/concert-details/concert-details.component';
+import {LoginComponent} from './components/login/login.component';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: LoginComponent },
+  { path: 'account', component: LoginComponent },
+  { path: 'artists', component: ArtistsComponent,
+    children: [
+      { path: ':key', component: ArtistDetailsComponent }
+    ]
+  },
+  { path: 'concerts', component: ConcertsComponent,
+    children: [
+      { path: ':key', component: ConcertDetailsComponent }
+    ]
+  },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ArtistsComponent } from './components/artists/artists.component';
+import { ArtistDetailsComponent } from './components/artists/artist-details/artist-details.component';
+import { ConcertsComponent } from './components/concerts/concerts.component';
+import { ConcertDetailsComponent } from './components/concerts/concert-details/concert-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ArtistsComponent,
+    ArtistDetailsComponent,
+    ConcertsComponent,
+    ConcertDetailsComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
