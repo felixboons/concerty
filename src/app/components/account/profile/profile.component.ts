cc5718a54e93ba40db5e1ae9f17e853392ff74f6
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-15 00:01:26 +0100

    Added _card styling for cards throughout the app

--- src/app/components/account/profile/profile.component.ts
+++ src/app/components/account/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.initializeForm();
   }
 
+  private initializeForm() {
+    this.form = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(500)
+      ]),
+      age: new FormControl(null, Validators.required)
+    })
+  }
 }
