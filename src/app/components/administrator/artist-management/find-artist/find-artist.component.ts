7b11147bb06f1f78fd3d2b023433c4390f6b7738
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-19 12:48:58 +0100

    Added searching functionality for artist management

--- src/app/components/administrator/artist-management/find-artist/find-artist.component.ts
+++ src/app/components/administrator/artist-management/find-artist/find-artist.component.ts
@@ -2,7 +2,6 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ArtistService} from '../../../../_services/artist.service';
 import {Artist} from '../../../../_models/artist.model';
 import {Genre} from '../../../../_enums/genre.enum';
-import * as jsonQuery from 'json-query';
 
 @Component({
   selector: 'app-find-artist',
@@ -17,8 +16,7 @@ export class FindArtistComponent implements OnInit {
 
   constructor(private artistService: ArtistService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   removeArtist(_id: string): void {
     this.artistService.deleteArtist(_id);
@@ -31,28 +29,36 @@ export class FindArtistComponent implements OnInit {
   search(): void {
     if (this.artistsCopy.length === 0) {
       this.artistsCopy = this.artists;
-      console.log(this.artistsCopy);
     }
 
+    // Only when there is valid input -> search.
     if (this.input !== '') {
-      // const regEx = new RegExp(`.*${this.input}.*/gi`);
-      // const query = `[*name=~${regEx}][*_id=~${regEx}]`;
+      const input = this.input.toLowerCase();
 
-      // TODO: MAKE THE REGEX MATCH WITH INPUT.
-      // - Regex check does work in this setup.
-      // - It just doesn't match with my input correctly.
-      const regEx = new RegExp(`.*${this.input}/gi`);
-      const query = `[*name=~${regEx}]`;
-      console.log(query);
-
-
-      this.artists = jsonQuery(query, {
-        data: this.artistsCopy,
-        allowRegexp: true
-      }).value;
-      console.log(this.artists);
+      this.artists = this.artists.filter(artist => {
+        return FindArtistComponent.idMatchesInput(artist._id, input) ||
+          FindArtistComponent.nameMatchesInput(artist.name, input) ||
+          FindArtistComponent.genreMatchesInput(artist.genre, input);
+      });
     } else {
+
+      // Show all artists.
       this.artists = this.artistsCopy;
     }
+  }
+
+  private static idMatchesInput(id: string, input: string): boolean {
+    id = id.toLowerCase();
+    return id.indexOf(input) > -1;
+  }
+
+  private static nameMatchesInput(name: string, input: string): boolean {
+    name = name.toLowerCase();
+    return name.indexOf(input) > -1;
+  }
+
+  private static genreMatchesInput(genre: Genre, input: string): boolean {
+    const genreString = Genre[genre].toLowerCase();
+    return genreString.indexOf(input) > -1;
   }
 }
