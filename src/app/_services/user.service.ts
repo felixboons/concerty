b166f8a6d9b05216b0923e577cb4096077137668
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-23 18:22:40 +0100

    Added currentUser to userService. Did not test yet

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../_models/user.model';
+import {Observable, Subject, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private readonly url = environment.serverUrlPrefix + 'users';
+  private currentUser: User = null;
+  currentUserSubject = new Subject<User>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject
+      .subscribe(user => this.currentUser = user);
+  }
 
   createUser(user: User): Promise<boolean> {
     const body = {
@@ -30,5 +37,15 @@ export class UserService {
           reject();
         });
     });
+  }
+
+
+  // TODO: Get user ID from token? Then do a call to server to retrieve data. Return observable and create subject etc etc
+  getUser(_id: string): Observable<User> {
+    return this.http.get(this.url + '/' + _id)
+      .pipe(map((response: User) => response),
+        catchError(err => {
+          return throwError('Server responded with unexpected object array type');
+        }));
   }
 }
