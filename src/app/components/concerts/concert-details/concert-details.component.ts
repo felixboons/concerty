7c5ed3a1986029f1af708d0409fd5e314bd04432
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-24 20:50:35 +0100

    tickets dont show when the concert is over

--- src/app/components/concerts/concert-details/concert-details.component.ts
+++ src/app/components/concerts/concert-details/concert-details.component.ts
@@ -4,6 +4,7 @@ import {Concert} from '../../../_models/concert.model';
 import {ActivatedRoute} from '@angular/router';
 import {Venue} from '../../../_enums/venue.enum';
 import {ArtistService} from '../../../_services/artist.service';
+import {DateHelper} from '../../../_helpers/date-helper';
 
 @Component({
   selector: 'app-concert-details',
@@ -13,6 +14,7 @@ import {ArtistService} from '../../../_services/artist.service';
 export class ConcertDetailsComponent implements OnInit {
   Venue = Venue;
   concert: Concert;
+  isUpcoming: boolean;
 
   constructor(private concertService: ConcertService,
               private artistService: ArtistService,
@@ -24,6 +26,7 @@ export class ConcertDetailsComponent implements OnInit {
       .subscribe(params => {
         const _id = params.key;
         this.concert = this.concertService.getConcert(_id);
+        this.isUpcoming = DateHelper.isUpcoming(this.concert.date);
       });
   }
 }
