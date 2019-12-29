394a5cd2a007d7080429928689a05e1647478b8a
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-29 16:55:57 +0100

    Footer now hides admin and user stuff when required.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment'
+import {AuthService} from '../../_services/auth.service';
+import {Role} from '../../_enums/role.enum';
 
 @Component({
   selector: 'app-footer',
@@ -7,13 +9,27 @@ import * as moment from 'moment'
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  Role = Role;
+  isAuthenticated = false;
+  isAdministrator = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.isAuthenticated = this.authService.isAuthenticated();
+    this.authService.isAuthenticatedSubject.subscribe(user => {
+      if (!!user) {
+        this.isAdministrator = Role[user.role] === Role.ADMIN;
+        this.isAuthenticated = true;
+      } else {
+        this.isAuthenticated = false;
+        this.isAdministrator = false;
+      }
+    });
   }
 
   getCurrentYear(): string {
     return moment().year().toString()
   }
+
 }
