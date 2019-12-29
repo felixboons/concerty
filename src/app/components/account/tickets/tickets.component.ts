bab0cc2c6e3164c02c206efdefc92735c45889d4
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-29 16:48:32 +0100

    Tickets in tickets component are onw clickable and will navigate user to respective ticket

--- src/app/components/account/tickets/tickets.component.ts
+++ src/app/components/account/tickets/tickets.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Ticket} from '../../../_models/ticket.model';
 import {AuthService} from '../../../_services/auth.service';
 import {TicketItem} from '../../../_models/ticket-item.model';
+import {DateHelper} from '../../../_helpers/date-helper';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-tickets',
@@ -10,19 +12,34 @@ import {TicketItem} from '../../../_models/ticket-item.model';
 })
 export class TicketsComponent implements OnInit {
   tickets: Ticket[] = [];
+  upcomingTickets: Ticket[] = [];
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
     this.authService.isAuthenticatedSubject
       .subscribe(user => {
         if (user) {
           this.tickets = user.tickets.reverse();
+          this.fillUpcomingTickets();
         }
       });
   }
 
   getTotalTickets(items: TicketItem[]): number {
     return Ticket.getTotalTickets(items);
+  }
+
+  fillUpcomingTickets(): void {
+    for (const ticket of this.tickets) {
+      if (DateHelper.isUpcoming(ticket.concert.date)) {
+        this.upcomingTickets.push(ticket);
+      }
+    }
+  }
+
+  showTicket(_id: string) {
+    this.router.navigate(['/account/tickets', _id])
   }
 }
