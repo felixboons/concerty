79b57db6eb54fc57cb55f7bc7d117cd370183c3c
ALTERNATE\Felix <felixboons@hotmail.com>
2020-03-13 16:45:26 +0100

    Pulled packakge-lock vulnrebility PR from remote. Added stuff from Avans example to Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/tastic-items-client'),
+      dir: require('path').join(__dirname, './coverage/concerty-client'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
@@ -32,7 +32,6 @@ module.exports = function (config) {
         flags: ['--no-sandbox']
       }
     },
-
     singleRun: false,
     restartOnFileChange: true
   });
